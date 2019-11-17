0a69f3f5a873116a4604cbe1a5ac8dcc5efd6be4
BADHON RAHMAN <badhon.mostafizur@gmail.com>
2019-11-17 06:00:03 +0600

    update bootStrap file

--- grails-app/init/practical/exam/BootStrap.groovy
+++ grails-app/init/practical/exam/BootStrap.groovy
@@ -1,5 +1,6 @@
 package practical.exam
 
+import practical.exam.AdminInitializationService
 class BootStrap {
     def init = { servletContext ->
         AdminInitializationService.initializeAdmin()
